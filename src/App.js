fd93b8a17929456f7f3d333309ef1b735c0aa12d
zahra ellioun <100999190+Zahra-ellioun@users.noreply.github.com>
2024-11-11 13:12:56 +0330

    delete task state in this file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import { Todolist, AddItem } from "./component/Index.jsx";
 
 const App = () => {
-  const [task,setTask]=useState({})
-  
   return (
     <>
       <Routes>
